dc419c0d38deb75ba0771f030191a655356735bc
PlanHorror <nmtxma2004@gmail.com>
2025-08-21 12:28:06 +0700

    feat: basic method proposal

--- src/proposal/proposal.module.ts
+++ src/proposal/proposal.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProposalService } from './proposal.service';
 import { ProposalController } from './proposal.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [ProposalService],
-  controllers: [ProposalController]
+  providers: [ProposalService, PrismaService],
+  controllers: [ProposalController],
+  exports: [ProposalService],
 })
 export class ProposalModule {}
--- src/proposal/proposal.service.ts
+++ src/proposal/proposal.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class ProposalService {}
+export class ProposalService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllProposals() {
+    return this.prisma.proposal.findMany();
+  }
+
+  async getProposalById(id: string) {
+    try {
+      const proposal = await this.prisma.proposal.findUnique({
+        where: { id },
+      });
+      if (!proposal) {
+        throw new NotFoundException('Proposal not found');
+      }
+      return proposal;
+    } catch {
+      throw new NotFoundException('Proposal not found');
+    }
+  }
+
+  async createProposal(data: any) {
+    try {
+      return await this.prisma.proposal.create({
+        data,
+      });
+    } catch {
+      throw new InternalServerErrorException('Error creating proposal');
+    }
+  }
+
+  async updateProposal(id: string, data: any) {
+    await this.getProposalById(id);
+    try {
+      const proposal = await this.prisma.proposal.update({
+        where: { id },
+        data,
+      });
+      return proposal;
+    } catch {
+      throw new InternalServerErrorException('Error updating proposal');
+    }
+  }
+
+  async deleteProposal(id: string) {
+    await this.getProposalById(id);
+    try {
+      await this.prisma.proposal.delete({
+        where: { id },
+      });
+    } catch {
+      throw new InternalServerErrorException('Error deleting proposal');
+    }
+  }
+}
