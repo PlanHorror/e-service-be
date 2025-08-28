808b068eca172688c3a36ef41842bab36ff83313
PlanHorror <nmtxma2004@gmail.com>
2025-08-28 14:36:29 +0700

    feat: swagger for dto

--- src/proposal/dto/proposal-query.dto.ts
+++ src/proposal/dto/proposal-query.dto.ts
@@ -1,10 +1,19 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ProposalQueryDto {
+  @ApiProperty({
+    description: 'Security code for proposal access',
+    example: 'SEC123456',
+  })
   @IsString()
   @IsNotEmpty()
   security_code: string;
 
+  @ApiProperty({
+    description: 'Proposal code identifier',
+    example: 'PROP001',
+  })
   @IsString()
   @IsNotEmpty()
   code: string;
